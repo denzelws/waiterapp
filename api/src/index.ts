8a177932e498d9c9b56ae3607d7c7d90671145b7
DenzelWashington <denzelwashingtonsantana@gmail.com>
2024-01-24 02:31:12 -0300

    Add mongoose connection

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,12 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
 const app = express()
+
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => console.log('Conectado ao mongo'))
+  .catch(() => console.log('erro'))
+
 const port = 3001
 
 app.listen(port, () => {
